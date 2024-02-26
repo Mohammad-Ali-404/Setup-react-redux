5066ebcbb3331b523a75170e6567e760924b93ba
wpmohammad1 <alimohammadjhd@gmail.com>
2024-02-27 00:00:15 +0600

    added rne

--- src/components/TodoList.jsx
+++ src/components/TodoList.jsx
@@ -1,33 +1,34 @@
 import React from 'react';
+import { useSelector } from 'react-redux';
+import { FaRegCheckCircle } from "react-icons/fa";
 import { RxCrossCircled } from "react-icons/rx";
 
 const TodoList = () => {
+    const todos = useSelector(state => state.todos);
+
     return (
         <div>
-            <ul className='border-t border-b px-2 flex justify-between items-center hover:bg-gray-100 hover:transition-all mb-4 space-x-4 border-gray-200 py-2'>
-                <div className='flex gap-4 items-center'>
-                    <li className='h-3 w-3 border-2 border-green-500 md:hover:bg-green-500 rounded-full cursor-pointer'></li>
-                    <p className='line-through'>Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
-                </div>
-                <div className='flex gap-4 items-center'>
-                    <li className='h-3 w-3 border-2 border-green-500 md:hover:bg-green-500 rounded-full cursor-pointer bg-green-500'></li>
-                    <li className='h-3 w-3 border-2 border-red-500 md:hover:bg-red-500 rounded-full cursor-pointer'></li>
-                    <li className='h-3 w-3 border-2 border-yellow-500 md:hover:bg-yellow-500 rounded-full cursor-pointer'></li>
-                    <RxCrossCircled className="text-base cursor-pointer" />
-                </div>
-            </ul> 
-            <ul className='border-t px-2 border-b flex justify-between items-center hover:bg-gray-100 hover:transition-all space-x-4 border-gray-200 py-2'>
-                <div className='flex gap-4 items-center'>
-                    <li className='h-3 w-3 border-2 border-green-500 md:hover:bg-green-500 rounded-full cursor-pointer'></li>
-                    <p className='line-through'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Accusantium, deleniti.</p>
-                </div>
-                <div className='flex gap-4 items-center'>
-                    <li className='h-3 w-3 border-2 border-green-500 md:hover:bg-green-500 rounded-full cursor-pointer bg-green-500'></li>
-                    <li className='h-3 w-3 border-2 border-red-500 md:hover:bg-red-500 rounded-full cursor-pointer'></li>
-                    <li className='h-3 w-3 border-2 border-yellow-500 md:hover:bg-yellow-500 rounded-full cursor-pointer'></li>
-                    <RxCrossCircled className="text-base cursor-pointer" />
-                </div>
-            </ul> 
+            {todos.map(todo => 
+                <div key={todo.id}>
+                    <ul className='border-t px-2 border-b flex justify-between items-center hover:bg-gray-100 hover:transition-all space-x-4 border-gray-200 py-2'>
+                        <div className='flex gap-4 items-center'>
+                            <li className=''>
+                                
+                                {todo.completed ? <FaRegCheckCircle className='text-green-600'/> : <div ><input className={`h-4 w-4 border-2 border-gray-400 rounded-full cursor-pointer ${todo.completed && <FaRegCheckCircle className='text-green-600'/>}`} name="" checked={todo.completed} id="" /></div>}
+                            </li>
+                            <p className={todo.completed ? 'line-through' : ''}>{todo.text}</p>
+                        </div>
+                        <div className='flex gap-4 items-center'>
+                                <>
+                                    <li className={`h-3 w-3 border-2 rounded-full cursor-pointer md:hover:bg-green-500 border-green-500 ${todo.color === 'green' && 'border-green-500  bg-green-500'} `}></li>
+                                    <li className={`h-3 w-3 border-2 rounded-full cursor-pointer md:hover:bg-yellow-500 border-yellow-500 ${todo.color === 'yellow' && 'border-yellow-500  bg-yellow-500'} `}></li>
+                                    <li className={`h-3 w-3 border-2 rounded-full cursor-pointer md:hover:bg-red-500 border-red-500  ${todo.color === 'red' && 'border-red-500 bg-red-500'} `}></li>
+                                    <RxCrossCircled className="text-base cursor-pointer" />
+                                </>
+                        </div>
+                    </ul>
+                </div>    
+            )}
         </div>
     );
 };
--- src/redux/todos/initialState.jsx
+++ src/redux/todos/initialState.jsx
@@ -3,15 +3,16 @@ export const initialState = [
         id:1,
         text: "Learn React Js",
         completed: true,
+        color:'green'
     },
     {
-        id:1,
+        id:2,
         text: "Learn Node js",
-        completed: false,
-        color: "red"
+        completed: true,
+        color: "yellow"
     },
     {
-        id:1,
+        id:3,
         text: "Learn Redux",
         completed: false,
         color: "red"
