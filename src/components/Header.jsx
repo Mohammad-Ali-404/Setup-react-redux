5b09fbb9403b3aad2c9c99cf187ccaaee0065690
wpmohammad1 <alimohammadjhd@gmail.com>
2024-02-28 15:17:46 +0600

    complete done

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,12 +1,29 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { PiNotePencilBold } from "react-icons/pi";
 import { GoPlusCircle } from "react-icons/go";
 import { IoCheckmarkDone } from "react-icons/io5";
-
+import { added, allCompleted, clearCompleted } from '../redux/todos/actions';
+import { useDispatch } from 'react-redux';
 const Header = () => {
+    const dispatch = useDispatch()
+    const [input, setInput] = useState('')
+    const handleInput = (e) =>{
+        setInput(e.target.value)
+    }
+    const submitHandler = (e) =>{
+        e.preventDefault()
+        dispatch(added(input))
+        setInput("")
+    }
+    const completeHandler = () =>{
+        dispatch(allCompleted())
+    }
+    const clearComplete = () =>{
+        dispatch(clearCompleted())
+    }
   return (
     <div>
-        <form className="bg-gray-100 text-white p-4 flex justify-between items-center">
+        <form onSubmit={submitHandler} className="bg-gray-100 text-white p-4 flex justify-between items-center">
             <div className="flex items-center px-4">
                 <PiNotePencilBold className="text-3xl text-black space-x-5"/>
             </div>
@@ -14,17 +31,19 @@ const Header = () => {
                 type="text"
                 className="w-full text-lg px-4 py-1 border-none outline-none bg-gray-200 text-gray-500"
                 placeholder='Type Your Todo'
+                value={input}
+                onChange={handleInput}
             />
             <button type='submit' className=" text-white font-bold py-2 px-4 ml-4 rounded">
                 <GoPlusCircle  className="text-3xl text-black space-x-5"/>
             </button>
         </form>
         <ul className='flex justify-between my-4 text-base text-gray-500'>
-            <li className='flex items-center space-x-1 cursor-pointer'>
+            <li className='flex items-center space-x-1 cursor-pointer' onClick={completeHandler}>
                 <IoCheckmarkDone/>
                 <span>Complete All Task</span>
             </li>
-            <li className='cursor-pointer'>
+            <li className='cursor-pointer' onClick={clearComplete}>
                 Clear Completed
             </li>
         </ul>
--- src/redux/todos/reducer.jsx
+++ src/redux/todos/reducer.jsx
@@ -5,7 +5,7 @@ import { initialState } from './initialState';
 import { ADDED, ALLCOMPLETED, CLEARCOMPLETED, COLORSELECTED, DELETED, TOGGLED } from './actionTypes';
 
 const nextTodoId = (todos) => {
-    const maxId = todos.reducer((maxId, todo) => Math.max(todo.id, maxId), -1)
+    const maxId = todos.reduce((maxId, todo) => Math.max(todo.id, maxId), -1);
     return maxId + 1;
 };
 
@@ -15,7 +15,9 @@ const reducer = (state = initialState, action) => {
             return [
                 ...state,
                 {
-                    id: nextTodoId(state)
+                    id: nextTodoId(state),
+                    text: action.payload,
+                    completed: false
                 }
             ]    
 
